2dccd0a29508e0e47963ddd2ab0d6c2573b49a33
Rodrigo <loroxeraco@hotmail.com>
2024-01-03 01:49:19 +0100

    Update models definition

--- shortener_app/models.py
+++ shortener_app/models.py
@@ -5,3 +5,6 @@ class Url(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     full_url = models.URLField()
     shortened_uri = models.URLField()
+
+    def __str__(self):
+        return(f'{self.shortened_uri} - {self.full_url}')
