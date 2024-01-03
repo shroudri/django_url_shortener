83ebda11753b6bb815a3e05495d585088ffa2536
Rodrigo <loroxeraco@hotmail.com>
2024-01-03 01:26:28 +0100

    Create Url model, and update project settings

--- shortener_app/models.py
+++ shortener_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Url(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    long_url = models.URLField()
+    short_url = models.URLField()
